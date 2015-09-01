8744b2634c8657e348ecaab69a0abe3eb8a12b0f
utsuro <tristanvdl@gmail.com>
2015-09-01 10:08:50 +0200

    nog nieuwerder

--- gallery/script.js
+++ gallery/script.js
@@ -1,35 +1,42 @@
 window.addEventListener("load",function(){
 
-var imgCounter = 0;
 var rowCounter = 0;
 
+var imgCounter = 0;
 var imgCounter1 = 0;
-var rowCounter1 = 0;
-
 var imgCounter2 = 0;
-var rowCounter2 = 0;
 
-var img = "img/"+imgCounter+"_"+rowCounter+".jpg";
-var source = document.getElementById("#thisImage");
+var img = "img/"+rowCounter+"_"+imgCounter+".jpg";
 
-var source1 = document.getElementById("#thisImage1");
-var source2 = document.getElementById("#thisImage2");
+var source = document.getElementById("thisImage");
+var source1 = document.getElementById("thisImage1");
+var source2 = document.getElementById("thisImage2");
 
-var left = document.getElementById("#previous");
-var right = document.getElementById("#next");
+var left = document.getElementById("previous");
+var right = document.getElementById("next");
 
-var left1 = document.getElementById("#previous1");
-var right1 = document.getElementById("#next1");
+var left1 = document.getElementById("previous1");
+var right1 = document.getElementById("next1");
 
-var left2 = document.getElementById("#previous2");
-var right2 = document.getElementById("#next2");
+var left2 = document.getElementById("previous2");
+var right2 = document.getElementById("next2");
 
 left.onclick = function(){
-	if (imgCounter < 0){
+	if (imgCounter == 0){
 		imgCounter = 12;
 	}
 	imgCounter--;
-	source.src = img;
+	img = "img/"+rowCounter+"_"+imgCounter+".jpg";
+	document.getElementById("thisImage").src = img;
+}
+
+right.onclick = function(){
+if (imgCounter == 12){
+		imgCounter = -1;
+	}
+	imgCounter++;
+	img = "img/"+rowCounter+"_"+imgCounter+".jpg";
+	document.getElementById("thisImage").src = img;
 
 }
 
