7386c36cacacc17efe579517d354f532c987c5de
dopcio <dopico1996@hotmail.com>
2015-09-14 12:28:39 +0200

    JE OMA

--- les3/script.js
+++ les3/script.js
@@ -1,13 +1,32 @@
-var person = {
-    firstName:"Idris",
-    lastName:"Dopico",
-    age:19,
-    eyeColor:"brown",
-    favouriteColour:"paars"
-};
+window.addEventListener("load", function(){
 
-Array.prototype.clear = function(){
-	this.length = 0;
-};
+	var person = {
+	    firstName:"Idris",
+	    lastName:"Dopico",
+	    age:19,
+	    eyeColor:"brown",
+	    favouriteColour:"paars"
+	};
 
-console.log(person.clear());
+
+	Array.prototype.clear = function(){
+		this.length = 0;
+
+	};
+	var age = [1,2,3,4,5];
+
+	console.log(age.length);
+	age.clear();
+	console.log(age.length);
+
+	var createNewUser = function(){
+		firstName = "Isabel",
+		lastName = "iets",
+		age = 17,
+		eyeColor = "groen?",
+		favouriteColour = "paars"		
+	};
+	return createNewUser;
+	debugger;
+});
+
